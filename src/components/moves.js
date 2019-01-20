11f786a29604f942d8a1f6a0b1167811c01c7262
Brandon Turner <brandontheturner@gmail.com>
2019-01-20 00:13:41 -0600

    updating css

--- src/components/moves.js
+++ src/components/moves.js
@@ -5,7 +5,7 @@ class Moves extends Component {
   render() {
     return (
       <div>
-        <h1>Moves for {this.props.data.name}</h1>
+        <h2>Moves for {this.props.data.name}</h2>
         {this.props.data.moves.map((item, i) => {
           return <p key={i}>{item.move.name}</p>;
         })}
